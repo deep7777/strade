0d4165dbba74d790ccf6774d5e0ebe1ac04914f3
Deepak Yadav <deepak@appnexio.com>
2022-10-22 09:05:11 +0530

    add sql

--- stocks.py
+++ stocks.py
@@ -23,33 +23,56 @@ print(dateval)
 mt = date.today().strftime('%m');
 day = date.today().strftime('%d');
 
-start = date(2022,8,1)
+start = date(2022,1,1)
 end = date(2022,int(mt),int(day))
 expiry_date = date(2022,10,27)
 
 
 cursor = conn.cursor()
 
-# Retrieving single row
-sql = 'SELECT cname,csymbol,id,options from companies order by id asc limit 1'
-# Executing the query
-cursor.execute(sql)
-# Fetching 1st row from the table
 
-result = cursor.fetchall()
+def insertStockData(dateval,symbol,series,prevclose,ropen,high,low,last,close,vwap,volume,turnover,trades,deliverablevolume,deliverable):
+    insert_stmt = (
+    	"INSERT INTO stockdata(dateval, symbol,series,prevclose,open,high,low,last,close,vwap,volume,turnover,trades,deliverablevolume,deliverable)" 
+    	"VALUES (%s, %s, %s,%s, %s,%s,%s,%s,%s,%s, %s, %s, %s,%s,%s)"
+    )
+    data = (dateval,symbol,series,prevclose,ropen,high,low,last,close,vwap,volume,turnover,trades,deliverablevolume,deliverable)
+    cursor.execute(insert_stmt,data)
+    conn.commit()
+
 
 def getstock(name):
 	nifty_opt = get_history(
-						symbol='TECHM',
+						symbol=name,
 		                start=start,
 		                end=end,
 		                index=False
 	                )
 	df = pd.DataFrame(nifty_opt)
-	print(df)
-	# for index, row in df.iterrows():
-	# 	print(row) 
+	for index, row in df.iterrows():
+		dateval=row.name
+		symbol=row[0]
+		series=row[1]
+		prevclose=row[2]
+		ropen=row[3]
+		high=row[4]
+		low=row[5]
+		last=row[6]
+		close=row[7]
+		vwap=row[8]
+		volume=row[9]
+		turnover=row[10]
+		trades=row[11]
+		deliverablevolume=row[12]
+		deliverable=row[13]
+		insertStockData(dateval,symbol,series,prevclose,ropen,high,low,last,close,vwap,volume,turnover,trades,deliverablevolume,deliverable)
 
+# Retrieving single row
+sql = 'SELECT cname,csymbol,id,options from companies where options=1 order by id asc'
+# Executing the query
+cursor.execute(sql)
+# Fetching 1st row from the table
+result = cursor.fetchall()
 for row in result:
     name = row[1]
     company = row[0]
